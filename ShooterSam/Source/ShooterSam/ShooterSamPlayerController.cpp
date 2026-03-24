192fc27dc7a3d9677660f9894263003e5f5e7d18
Andrii Kreidun <kreyduna@gmail.com>
2026-03-24 12:17:32 +0200

    add character, movement and crosshair

--- ShooterSam/Source/ShooterSam/ShooterSamPlayerController.cpp
+++ ShooterSam/Source/ShooterSam/ShooterSamPlayerController.cpp
@@ -31,6 +31,11 @@ void AShooterSamPlayerController::BeginPlay()
 		}
 
 	}
+	HUDWidget = CreateWidget<UHUDWidget>(this, HUDWidgetClass);
+	if (HUDWidget)
+	{
+		HUDWidget->AddToViewport();
+	}
 }
 
 void AShooterSamPlayerController::SetupInputComponent()
--- ShooterSam/Source/ShooterSam/ShooterSamPlayerController.h
+++ ShooterSam/Source/ShooterSam/ShooterSamPlayerController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "HUDWidget.h"
 #include "ShooterSamPlayerController.generated.h"
 
 class UInputMappingContext;
@@ -48,5 +49,10 @@ protected:
 
 	/** Returns true if the player should use UMG touch controls */
 	bool ShouldUseTouchControls() const;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UHUDWidget> HUDWidgetClass;
+	UPROPERTY(VisibleAnywhere)
+	UHUDWidget* HUDWidget;
 
 };
